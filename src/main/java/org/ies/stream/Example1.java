fd12f1cc7dd7c574c2837b1cbed2f1be6cedb795
Jesus <jesus@b25>
2025-03-24 09:45:45 +0100

    ejercicio 1 2 3 4

--- src/main/java/org/ies/stream/Example1.java
+++ src/main/java/org/ies/stream/Example1.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Example1 {
     public static void main(String[] args) {
         List<String> stringList = List.of("Bob", "Peppa", "George");
+        List<Integer> intList = List.of(23, 78, 89);
         stringList
                 .stream()
                 .forEach(System.out::println);
        Optional<String> primero = fist(stringList);
         System.out.println(primero);
+        List<Integer> newList = sum10(intList);
+        System.out.println(newList);
     }
     public static <T> Optional<T> fist(List<T> list){
         return list
@@ -31,4 +36,11 @@ public class Example1 {
                 .skip(list.size()-1)
                 .findFirst();
     }
+    public static List<Integer> sum10( List<Integer> list){
+        return list
+                .stream()
+                .map(n -> n +10)
+                .collect(Collectors.toList());
+    }
 }
+
