d41ec48b8f14f6f98bc8fa52374d23b33df41b9b
Jesus <jesus@b25>
2025-03-24 09:14:37 +0100

    empezando

--- src/main/java/org/ies/stream/Example1.java
+++ src/main/java/org/ies/stream/Example1.java
@@ -1,4 +1,34 @@
 package org.ies.stream;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
 public class Example1 {
+    public static void main(String[] args) {
+        List<String> stringList = List.of("Bob", "Peppa", "George");
+        stringList
+                .stream()
+                .forEach(System.out::println);
+       Optional<String> primero = fist(stringList);
+        System.out.println(primero);
+    }
+    public static <T> Optional<T> fist(List<T> list){
+        return list
+                .stream()
+                .findFirst();
+    }
+    public static <T> Optional<T> second(List<T> list){
+        return list
+                .stream()
+                .skip(1)
+                .findFirst();
+    }
+    public static <T> Optional<T> last(List<T> list){
+        return list
+                .stream()
+                .skip(list.size()-1)
+                .findFirst();
+    }
 }
